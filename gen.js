43155ee70986971758fc928d75de9ecde1f0120a
DaniGuardiola <contacto@daniguardiola.me>
2015-04-28 03:20:01 +0200

    Lot of improvements and I should be sleeping again

--- gen.js
+++ gen.js
@@ -1,14 +1,13 @@
 "use strict";
 var fs = require("fs-extra");
+var jsdom = require("jsdom");
 
 var data, special;
-var replaceRegex = /("!)?%([a-zA-Z]+)%(!")?/g;
+var replaceRegex = /("!)?%([a-zA-Z\.]+)%(!")?/g;
 
 
 function generate(data) {
-    // console.log(data);
-
-    var name = data.dirName;
+    var name = data.meta.dir;
     var product = "products/" + name + "/";
     var template = "product-template/";
     var backup = "backups/" + name + "/";
@@ -31,36 +30,142 @@ function generate(data) {
 
     /* Transform HTML */
     var html = fs.readFileSync(product + "index.html");
-    // console.log(html + "");
     html = (html + "").replace(replaceRegex, replacer);
-    // console.log(html + "");
     fs.writeFileSync(product + "index.html", html);
 
     /* Transform JavaScript */
     var js = fs.readFileSync(product + "js/script.js");
-    // console.log(js + "");
     js = (js + "").replace(replaceRegex, replacer);
-    // console.log(js + "");
 
     fs.writeFileSync(product + "js/script.js", js);
 }
 
+function genLandingSections() {
+    jsdom.env("", function(errors, window) {
+        var jsdom = require("jsdom").jsdom;
+
+        var window = jsdom().parentWindow;
+        var document = window.document;
+
+        // Vars
+        var i;
+        var wrap;
+
+        // Landing vars
+        var landing;
+        var section;
+        var tile;
+        var text;
+        var image;
+
+        // Changelog vars
+        var changelog;
+        var version;
+        var p;
+        var title;
+        var change;
+
+        // Landing generator (incomplete)
+        landing = document.createElement("div");
+        landing.className = "landing";
+
+        for (i = 0; i < data.landing.sections.length; i++) {
+            section = data.landing.sections[i];
+
+            if (section.text) {
+                text = document.createElement("span");
+            }
+
+            tile = document.createElement("div");
+
+            text = "";
+        }
+
+        // Changelog generator
+        changelog = document.createElement("div");
+        changelog.className = "changelog";
+
+        for (i = data.changelog.versions.length - 1; i >= 0; i--) {
+            version = data.changelog.versions[i];
+
+            p = document.createElement("p");
+            p.setAttribute("md-padding", "");
+            p.setAttribute("data-version", version.number);
+
+
+            title = document.createElement("span");
+            title.setAttribute("md-typo", "title");
+            title.innerHTML = version.number + "<br>";
+
+            change = document.createElement("span");
+            change.innerHTML = version.changes.join("<br>");
+
+            p.appendChild(title);
+            p.appendChild(change);
+
+            changelog.appendChild(p);
+        }
+
+        // Landing HTML extraction
+        wrap = document.createElement("div");
+        wrap.appendChild(landing);
+
+        // Landing export 
+        special.landing.html = wrap.innerHTML;
+
+        // Changelog HTML extraction
+        wrap.innerHTML = "";
+        wrap.appendChild(changelog);
+
+        // Changelog export 
+        special.changelog.html = wrap.innerHTML;
+
+        generate(data);
+    });
+}
+
 function genSpecial(data) {
-    var special = {
-        "currentVersion": data.changelog[data.changelog.length - 1].number
+    special = {
+        "version": data.changelog.versions[data.changelog.versions.length - 1].number,
+        "landing": {
+            "html": ""
+        },
+        "changelog": {
+            "html": "",
+            "theme": data.theme
+        }
     };
     special.javascript = {
         "changelog": data.changelog,
-        "version": special.currentVersion
+        "version": special.version
     };
+    genLandingSections();
 
     return special;
 }
 
+function objString(obj, is, value) {
+    if (!obj) {
+        return false;
+    }
+    if (typeof is === "string") {
+        return objString(obj, is.split("."), value);
+    } else if (is.length === 1 && value !== undefined) {
+        obj[is[0]] = value;
+        return value;
+    } else if (is.length === 0) {
+        return obj;
+    } else {
+        return objString(obj[is[0]], is.slice(1), value);
+    }
+}
+
 function replacer(a, b, name) {
-    var output = special[name] || data[name] || "ERROR: [" + name + "] does not exists!";
+    var output = objString(special, name) || objString(data, name) || "ERROR: [" + name + "] does not exists";
     if (typeof output === "object") {
         output = JSON.stringify(output);
+    } else if (a.indexOf("!") > -1) {
+        output = "\"" + output + "\"";
     }
     return output;
 }
@@ -83,6 +188,5 @@ for (var i = products.length - 1; i >= 0; i--) {
     if (!fs.statSync("data/" + products[i]).isDirectory()) {
         data = fs.readJSONSync("data/" + products[i]);
         special = genSpecial(data);
-        generate(data);
     }
 }
--- product-template/js/script.js
+++ product-template/js/script.js
@@ -8,4 +8,40 @@ var data = "!%javascript%!";
 
 function init() {
     paperkit.init();
+
+    var hash = window.location.hash.split("?")[0];
+    var tabs = document.querySelector("#main-paper md-tabbar");
+    var p;
+    var landingTitle = document.getElementById("landing-title");
+    var version = document.getElementById("version");
+    var changelog = document.querySelector("#main-paper .changelog");
+
+    function param(query) {
+        var url = window.location.href;
+        var re = new RegExp("/.*[?&]" + query + "=([^&]+)(&|$)");
+        var match = url.match(re);
+        return (match ? match[1] : "");
+    }
+
+    if (param("version")) {
+        if (param("version") === "latest") {
+            p = changelog.querySelector("p:first-of-type");
+        } else {
+            p = changelog.querySelector("p[data-version=\"" + param("version") + "\"]");
+        }
+        if (p) {
+            version.textContent = p.getAttribute("data-version");
+            p.setAttribute("md-color", "!%theme.color%!" + "-700");
+        }
+    }
+
+    if (hash === "#changelog") {
+        if (param("update") === "true") {
+            landingTitle.textContent = "!%changelog.landing%!";
+        }
+        setTimeout(function() {
+            tabs.children[1].click();
+        }, 3000);
+    }
+
 }
