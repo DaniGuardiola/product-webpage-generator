c8197281c41429eb69cbfc83a273b1f2b9bfd0c8
DaniGuardiola <contacto@daniguardiola.me>
2015-04-30 20:58:36 +0200

    Added some things

--- product-template/js/script.js
+++ product-template/js/script.js
@@ -6,6 +6,16 @@ window.addEventListener("load", function() {
 
 var data = "!%javascript%!";
 
+function addWaves() {
+    var buttons = document.querySelectorAll("md-icon-button");
+
+    for (var i = buttons.length - 1; i >= 0; i--) {
+        Waves.attach(buttons[i], ".button" ["waves-circle"]);
+    }
+
+    Waves.init();
+}
+
 function init() {
 
     var hash = window.location.hash.split("?")[0];
@@ -19,6 +29,8 @@ function init() {
     var landingHTML = document.getElementById("landing-html");
     var version = document.getElementById("version");
     var changelog = document.querySelector("#main-paper .changelog");
+
+    window.location.hash = "";
 
     function param(query) {
         var url = window.location.href;
@@ -48,6 +60,7 @@ function init() {
         tabs.classList.add("no-landing");
     }
 
+    addWaves();
     paperkit.init();
 
     if (hash === "#changes") {
@@ -59,10 +72,13 @@ function init() {
             tabs.children[1].click();
         }
     }
-
 }
 
 
-function donate() {
-    window.open("!%meta.donate%!");
+function donate(fab) {
+    fab.classList.add("donated");
+    fab.querySelector("img").src = "template/thanks.png";
+    setTimeout(function() {
+        window.open("!%meta.donate%!");
+    }, 500);
 }
